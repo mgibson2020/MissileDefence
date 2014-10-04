03e2b52bc90c64e367964ba4c7a6ebd2f208a0ce
Michael Gibson <mgibson2020@yahoo.com>
2014-10-04 13:03:05 -0700

    Two objects added
    GameObject and Building skeletons made.

---
+++ src/Building.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+
+public class Building extends GameObject {
+	
+	public Building() {
+		setHealth(3);
+		setMaxHealth(3);
+	}
+	
+	private void updLook() {
+		
+	}
+	
+	public void update(int u) {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+}
--- src/GameObject.java
+++ src/GameObject.java
@@ -1,4 +1,43 @@
+import java.awt.Graphics;
 
 public abstract class GameObject {
+	private int maxHealth = 1;
+	private int health = getMaxHealth();
+	
+	private void onDeath() {
+		
+	}
+	
+	private void updLook() {
+		
+	}
+	
+	public int getHealth() {
+		return health;
+	}
 
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public void remHealth() {
+		
+	}
+	
+	public void update(int u) {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+	
 }
