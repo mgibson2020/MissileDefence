1fb81b29d6eb52985d0a1a469f4185c1ab7e0863
Quiteblank <iamquiteblank@gmail.com>
2014-10-08 17:10:38 -0700

    Fixed Sound Error

--- src/Sound.java
+++ src/Sound.java
@@ -2,5 +2,5 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 
 public class Sound {
-	public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("/snd/gameover.wav"));
+	public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
 }
